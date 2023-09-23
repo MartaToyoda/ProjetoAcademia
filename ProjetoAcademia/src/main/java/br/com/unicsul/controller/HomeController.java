75e9ac59a68b08d8281536d333ea3341d45734ed
Rian <rianmendonca@proton.me>
2023-09-22 23:26:15 -0300

    fixing

--- ProjetoAcademia/src/main/java/br/com/unicsul/controller/HomeController.java
+++ ProjetoAcademia/src/main/java/br/com/unicsul/controller/HomeController.java
@@ -3,7 +3,6 @@ package br.com.unicsul.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
