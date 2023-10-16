5231bbf0313805b31d0d9a2c5cb5f084c9ba0eb6
Rian Mendonça <rianmendonca@proton.me>
2023-10-16 15:06:13 -0300

    Adição da classe SpringTimeZoneConfig

---
+++ ProjetoAcademia/src/main/java/br/com/unicsul/configuration/SpringTimeZoneConfig.java
@@ -0,0 +1,11 @@
+package br.com.unicsul.configuration;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
+public class SpringTimeZoneConfig {
+    @PostConstruct
+    public void timeZoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
